a267fde070775a68ed81d7e988128a681aa3ced7
wojhan <w.haniewski@gmail.com>
2018-11-12 21:25:16 +0100

    Added button and blood animations, fixed rendering rank

---
+++ Assets/Scripts/BloodAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodAnimation : MonoBehaviour {
+
+    private int randInt;
+    private int currentInt = 0;
+    private float minScale = 0.2f;
+    private float maxScale = 1f;
+
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        randInt = Random.Range(0, 1000);
+        float scale = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(scale, scale, 1);
+        startPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (transform.localPosition.y < -240 || currentInt <= randInt)
+        {
+            float scale = Random.Range(minScale, maxScale);
+            transform.localPosition = startPos;
+            transform.localScale = new Vector3(scale, scale, 1);
+        }
+
+        if(currentInt <= randInt)
+        {
+            currentInt++;
+        }
+
+    }
+}
---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    private Vector3 defaultTransform;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(defaultTransform.x + 0.04f, defaultTransform.y + 0.04f, defaultTransform.z);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = defaultTransform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        defaultTransform = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
